95e428fd96c8199f7ba03ab59c7d18a641984e30
Andrey Sverkunov <andreysverkunov@Admins-MacBook-Pro.local>
2019-11-13 16:37:19 +0100

    improve method call

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,6 +1,6 @@
-import { Component, ContentChild, AfterContentInit, OnDestroy, OnInit, ElementRef, Input } from '@angular/core';
-import { of, Subscription } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { Component, ContentChild, AfterContentInit, OnInit, ElementRef, Input } from '@angular/core';
+import { of } from 'rxjs';
+import { delay, take } from 'rxjs/operators';
 import { Chart } from 'angular-highcharts';
 import { SeriesLineOptions } from 'highcharts';
 import { LoaderComponent } from '../loader/loader.component';
@@ -10,11 +10,10 @@ import { LoaderComponent } from '../loader/loader.component';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent implements OnInit, AfterContentInit, OnDestroy {
+export class ChartComponent implements OnInit, AfterContentInit {
   @Input() chartColor: string;
   chart: Chart;
   chartName = 'alligator 🐸';
-  private subscription: Subscription;
 
   @ContentChild(LoaderComponent, {static: true}) loader: LoaderComponent;
   @ContentChild(LoaderComponent, {static: true, read: ElementRef}) loaderRef: ElementRef;
@@ -41,8 +40,8 @@ export class ChartComponent implements OnInit, AfterContentInit, OnDestroy {
   }
 
   private initChartData() {
-    this.subscription = of<SeriesLineOptions>({name: this.chartName, data: [2.5, 2, 3, 2, 3, 2, 2], type: 'line'})
-      .pipe(delay(1000))
+    of<SeriesLineOptions>({name: this.chartName, data: [2.5, 2, 3, 2, 3, 2, 2], type: 'line'})
+      .pipe(delay(1000), take(1))
       .subscribe((chartValue: SeriesLineOptions) => {
         this.chart.addSeries(chartValue, true, true);
         this.loaderRef.nativeElement.setAttribute('class', 'loaded');
@@ -53,12 +52,6 @@ export class ChartComponent implements OnInit, AfterContentInit, OnDestroy {
     if (this.loader) {
       this.loader.loadingMessage = `Please wait for ${this.chartName} will be loaded`;
       this.loaderRef.nativeElement.setAttribute('style', `background-color: ${this.chartColor}77; color: ${this.chartColor}`);
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
     }
   }
 
